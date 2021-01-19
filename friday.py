397cf4d4a1d68b6373ad7c2f310a698993f11109
alenj <alenjojo252001@gmail.com>
2021-01-19 23:28:08 +0530

    All

--- friday.py
+++ friday.py
@@ -10,6 +10,7 @@ import time
 import os  # to remove created audio files
 import colorama
 import sys
+import pyttsx3 as p
 voice_data = ''
 
 
@@ -52,18 +53,26 @@ def record_audio(ask=False):
 
 
 def speak(audio_string):
-    tts = gTTS(text=audio_string, lang='en')  # text to speech
+    # tts = gTTS(text=audio_string, lang='en')  # text to speech
+    # print(f"Friday: {audio_string}")
+    # r = random.randint(1, 20000000)
+    # audio_file = 'audio' + str(r) + '.mp3'
+    # tts.save(audio_file)  # save as mp3
+    # playsound.playsound(audio_file)  # play the audio file
+    # os.remove(audio_file)  # remove audio file
+    engine = p.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[1].id)
     print(f"Friday: {audio_string}")
-    r = random.randint(1, 20000000)
-    audio_file = 'audio' + str(r) + '.mp3'
-    tts.save(audio_file)  # save as mp3
-    playsound.playsound(audio_file)  # play the audio file
-    os.remove(audio_file)  # remove audio file
+    engine.say(audio_string)
+    engine.runAndWait()
 
 
 def respond(voice_data):
+    i = 1
     # 1: greeting
     if there_exists(['hey', 'hi', 'hello', 'sup']):
+        i = 0
         greetings = [f"hey, how can I help you {person_obj.name}", f"hey, what's up? {person_obj.name}",
                      f"I'm listening {person_obj.name}", f"how can I help you? {person_obj.name}", f"hello {person_obj.name}"]
         greet = greetings[random.randint(0, len(greetings)-1)]
@@ -71,22 +80,26 @@ def respond(voice_data):
 
     # 2: name
     if there_exists(["what is your name", "what's your name", "tell me your name"]):
+        i = 0
         if person_obj.name:
             speak("my name is Friday")
         else:
             speak("my name is Friday. what's your name?")
 
     if there_exists(["my name is"]):
+        i = 0
         person_name = voice_data.split("is")[-1].strip()
         speak(f"okay, i will remember that {person_name}")
         person_obj.setName(person_name)
 
     # 3: greeting
     if there_exists(["how are you", "how are you doing"]):
+        i = 0
         speak(f"I'm very well, thanks for asking {person_obj.name}")
 
     # 4: time
     if there_exists(["what's the time", "tell me the time", "what time is it"]):
+        i = 0
         time = ctime().split(" ")[3].split(":")[0:2]
         if time[0] == "00":
             hours = '12'
@@ -98,6 +111,7 @@ def respond(voice_data):
 
     # 5: search google
     if there_exists(["search for"]) and 'youtube' not in voice_data:
+        i = 0
         search_term = voice_data.split("for")[-1]
         url = f"https://google.com/search?q={search_term}"
         webbrowser.get().open(url)
@@ -105,17 +119,21 @@ def respond(voice_data):
 
     # 6: search youtube
     if there_exists(["youtube"]):
+        i = 0
         search_term = voice_data.split("for")[-1]
         url = f"https://www.youtube.com/results?search_query={search_term}"
         webbrowser.get().open(url)
         speak(f'Here is what I found for {search_term} on youtube')
 
     if there_exists(["exit", "quit", "goodbye"]):
-        speak("Ok goodbye")
+        i = 0
+        speak("Okay goodbye")
         exit()
 
+    if i == 1:
+        i = 0
+        print("Invalid Command")
 
-time.sleep(1)
 
 person_obj = person()
 while(1):
